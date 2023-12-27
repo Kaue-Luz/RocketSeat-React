144dc5f8f29198beaf88708b234cfec46f9314e5
Erik Kauê <kaue_erik122@hotmail.com>
2023-12-27 17:01:29 -0300

    Estrutura da aplicação - Componente: Sidebar

--- 01-fundamentos-reactjs]/src/App.jsx
+++ 01-fundamentos-reactjs]/src/App.jsx
@@ -4,19 +4,27 @@ import { Header } from "./modules/Header.jsx"
 
 import './global.css'
 
+import styles from "./App.module.css";
+import { Sidebar } from "./modules/Sidebar.jsx";
+
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post 
-        author="Erik Kaue"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo adipisci ratione in error enim sunt reprehenderit incidunt veritatis omnis iure, ullam exercitationem reiciendis sed! Quidem quibusdam saepe vero magni distinctio?"
-      />
-      <Post 
-        author="Kaue erik"
-        content="outro post"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Erik Kaue"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo adipisci ratione in error enim sunt reprehenderit incidunt veritatis omnis iure, ullam exercitationem reiciendis sed! Quidem quibusdam saepe vero magni distinctio?"
+          />
+          <Post 
+            author="Kaue erik"
+            content="outro post"
+          />
+        </main>
+      </div>
     </div>
   )
 }
