b751f27124ae5a94c51c98d15b4259bdbf92a4a2
Erik Kauê <kaue_erik122@hotmail.com>
2023-12-27 14:38:28 -0300

    Estrutura da Aplicação - CSS Modules

--- 01-fundamentos-reactjs]/src/App.jsx
+++ 01-fundamentos-reactjs]/src/App.jsx
@@ -1,9 +1,14 @@
 // JSX - JavaScript + XML
 import { Post } from "./Post.jsx"
+import { Header } from "./modules/Header.jsx"
+
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post 
         author="Erik Kaue"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo adipisci ratione in error enim sunt reprehenderit incidunt veritatis omnis iure, ullam exercitationem reiciendis sed! Quidem quibusdam saepe vero magni distinctio?"
---
+++ 01-fundamentos-reactjs]/src/modules/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+console.log(styles);
+
+export function Header (){
+    return (
+        <header className={styles.header}>sla oq</header>
+    );
+}
