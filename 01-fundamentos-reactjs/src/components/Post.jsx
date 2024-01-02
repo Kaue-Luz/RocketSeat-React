cde8c1909ecbfc18c63ba5f33bb9f3f351dba2c4
Erik Kauê <kaue_erik122@hotmail.com>
2024-01-02 20:50:42 -0300

    Estrutura da aplicação - Componente: Avatar

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post () {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https:github.com/kaue-luz.png"/>
+                    <Avatar src="https:github.com/FeBassetto.png" />
                     <div className={styles.authorInfo}>
                         <strong>Erik Kaue</strong>
                         <span>Web Developer</span>
