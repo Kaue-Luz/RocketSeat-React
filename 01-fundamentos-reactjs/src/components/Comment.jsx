3a68664a24315680ebbd8f89dd124b548f8ab72b
Erik Kauê <kaue_erik122@hotmail.com>
2024-01-02 19:03:07 -0300

    Estrutura da aplicação - Componente: Comment

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import {ThumbsUp, Trash} from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https:github.com/kaue-luz.png" alt=''/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Erik Kaue</strong>
+                            <time title="19 de Maio às 09:00h" dateTime="2023-05-19 09:00:24">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Like <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
