fb647aa2b5ccd5c21bed6fd6855a78322729cd98
Erik Kauê <kaue_erik122@hotmail.com>
2024-01-03 15:35:45 -0300

    Os motores do React - Iterando no JSX

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 // JSX - JavaScript + XML
 import { Sidebar } from "./components/Sidebar.jsx";
 import { Header } from "./components/Header.jsx"
@@ -8,6 +9,40 @@ import './global.css'
 
 import styles from "./App.module.css";
 
+// author: {avatarUrl: "", name: "", role: ""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+    {
+      id: 1,
+      author: {
+        avatarUrl: 'https://github.com/kaue-luz.png',
+        name: 'Erik Kaue',
+        role: 'WEB Developer'
+      },
+      content: [
+        { type: 'paragraph', content: 'Fala galeraa 👋', },
+        { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+        { type: 'link', content: 'jane.design/doctorcare' },
+      ],
+      publishedAt: new Date('2021-01-03 20:00:00'),
+    },
+    {
+      id: 2,
+      author: {
+        avatarUrl: 'https://github.com/FeBassetto.png',
+        name: 'Felipe Basseto',
+        role: 'WEB Developer'
+      },
+      content: [
+        { type: 'paragraph', content: 'Fala galeraa 👋', },
+        { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+        { type: 'link', content: 'jane.design/doctorcare' },
+      ],
+      publishedAt: new Date('2021-01-01 20:00:00'),
+    }
+];
 
 export function App() {
   return (
@@ -17,8 +52,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author= {post.author}
+                content= {post.content}
+                publishedAt= {post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
