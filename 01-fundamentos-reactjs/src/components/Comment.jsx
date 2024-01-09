e36d86113333a207070c35d0a4f8664e20344847
Erik Kauê <kaue_erik122@hotmail.com>
2024-01-09 15:59:12 -0300

    Os motores do React - Realizando likes nos comentários

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,40 +1,49 @@
 /* eslint-disable react/prop-types */
-import {ThumbsUp, Trash} from 'phosphor-react'
-import styles from './Comment.module.css'
-import { Avatar } from './Avatar'
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
+import { useState } from "react";
 
-export function Comment({content, onDeleteComment}) {
-    function deleteComment(){
-        onDeleteComment(content)
-    }
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
 
-    return (
-        <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https:github.com/kaue-luz.png" />
+  function deleteComment() {
+    onDeleteComment(content);
+  }
 
-            <div className={styles.commentBox}>
-                <div className={styles.commentContent}>
-                    <header>
-                        <div className={styles.authorAndTime}>
-                            <strong>Erik Kaue</strong>
-                            <time title="19 de Maio às 09:00h" dateTime="2023-05-19 09:00:24">Cerca de 1h atrás</time>
-                        </div>
+  function likeComment () {
+    setLikeCount(likeCount + 1)
+  }
 
-                        <button onClick={deleteComment} title='Deletar comentário'>
-                            <Trash size={24}/>
-                        </button>
-                    </header>
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https:github.com/kaue-luz.png" />
 
-                    <p>{content}</p>
-                </div>
-
-                <footer>
-                    <button>
-                        <ThumbsUp />
-                        Like <span>20</span>
-                    </button>
-                </footer>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Erik Kaue</strong>
+              <time title="19 de Maio às 09:00h" dateTime="2023-05-19 09:00:24">
+                Cerca de 1h atrás
+              </time>
             </div>
+
+            <button onClick={deleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
         </div>
-    )
+
+        <footer>
+          <button onClick={likeComment}>
+            <ThumbsUp />
+            Like <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
